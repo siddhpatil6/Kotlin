16bee0c0ce027e6d432d34565079782dc2ab98d9
siddhantp <martand.joshi@loylty.com>
2017-11-30 14:02:07 +0530

    second commit

--- Sample Kotlin Demo/app/src/main/java/com/kotlin/siddhant/sampledemokotlin/KotlinActivity.kt
+++ Sample Kotlin Demo/app/src/main/java/com/kotlin/siddhant/sampledemokotlin/KotlinActivity.kt
@@ -10,16 +10,18 @@ import android.widget.Toast
 class KotlinActivity : AppCompatActivity() {
 
 
-
+    var button:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
 
+        button = findViewById<Button>(R.id.button)
+        button?.setOnClickListener {
+            Toast.makeText(this,"Hey Kotlin",Toast.LENGTH_LONG).show() // no semicolon
+        }
+
 
 
     }
-
-
-
 }
