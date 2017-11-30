442b975ecdccb9fef6b5d7714168d4ee1ea6f60e
siddhantp <martand.joshi@loylty.com>
2017-11-30 13:58:58 +0530

    first commit

---
+++ Sample Kotlin Demo/app/src/main/java/com/kotlin/siddhant/sampledemokotlin/KotlinActivity.kt
@@ -0,0 +1,25 @@
+package com.kotlin.siddhant.sampledemokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import android.widget.Button
+import android.widget.Toast
+
+
+class KotlinActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+
+
+
+    }
+
+
+
+}
